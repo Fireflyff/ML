25ba4dbac74f8a6591483920add311b72cbbd1b5
yingying <1582137770@qq.com>
2022-10-19 11:29:13 +0800

    add some data

--- 02_NLP/nlp_name_classification.py
+++ 02_NLP/nlp_name_classification.py
@@ -22,3 +22,138 @@ def unicodeToAscii(s):
         if unicodedata.category(c) != 'Mn'
         and c in all_letters
     )
+
+
+data_path = "../NLP_datasets/human_name/data/names/"
+
+
+def readLines(filename):
+    """从文件中读取每一行加载在内存中形成列表"""
+    # 打开指定文件并读取所有内容，使用strip()去除两侧空白符，然后以'\n'进行切分
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    # 对应每一个lines列表中的名字进行Ascii转换，使其规范化，最后返回一个名字列表
+    return [unicodeToAscii(line) for line in lines]
+
+
+category_lines = {}
+all_category = []
+for filename in glob.glob(data_path + '*.txt'):
+    category = filename.split('/')[-1].split('.')[0]
+    all_category.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+
+def lineToTensor(line):
+    """将所有人名转化为onehot张量表示"""
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][all_letters.find(letter)] = 1
+    return tensor
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers):
+        """初始化函数中有4个参数, 分别代表RNN输入最后一维尺寸, RNN的隐层最后一维尺寸, RNN层数"""
+        super(RNN, self).__init__()
+        # 将hidden_size与num_layers传入其中
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        # 实例化预定义的nn.RNN, 它的三个参数分别是input_size, hidden_size, num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers)
+        # 实例化nn.Linear, 这个线性层用于将nn.RNN的输出维度转化为指定的输出维度
+        self.linear = nn.Linear(hidden_size, output_size)
+        # 实例化nn中预定的Softmax层, 用于从输出层获得类别结果
+        self.softmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self, input, hidden):
+        """完成传统RNN中的主要逻辑, 输入参数input代表输入张量, 它的形状是1 x n_letters
+           hidden代表RNN的隐层张量, 它的形状是self.num_layers x 1 x self.hidden_size"""
+        # 因为预定义的nn.RNN要求输入维度一定是三维张量, 因此在这里使用unsqueeze(0)扩展一个维度
+        input = input.unsqueeze(0)
+        # 将input和hidden输入到传统RNN的实例化对象中，如果num_layers=1, rr恒等于hn
+        rr, hn = self.rnn(input, hidden)
+        # 将从RNN中获得的结果通过线性变换和softmax返回，同时返回hn作为后续RNN的输入
+        return self.softmax(self.linear(rr)), hn
+
+    def initHidden(self):
+        """初始化隐层张量"""
+        # 初始化一个（self.num_layers, 1, self.hidden_size）形状的0张量
+        return torch.zeros(self.num_layers, 1, self.hidden_size)
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
+        """初始化函数的参数与传统RNN相同"""
+        super(LSTM, self).__init__()
+        # 将hidden_size与num_layers传入其中
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        # 实例化预定义的nn.LSTM
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers)
+        self.linear = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self, input, hidden, c):
+        """在主要逻辑函数中多出一个参数c, 也就是LSTM中的细胞状态张量"""
+        # 使用unsqueeze(0)扩展一个维度
+        input = input.unsqueeze(0)
+        rr, (hn, c) = self.lstm(input, (hidden, c))
+        return self.softmax(self.linear(rr)), hn, c
+
+    def initHiddenAndC(self):
+        """初始化函数不仅初始化hidden还要初始化细胞状态c, 它们形状相同"""
+        c = hidden = torch.zeros(self.num_layers, 1, self.hidden_size)
+        return hidden, c
+
+
+# GRU与传统RNN的外部形式相同, 都是只传递隐层张量, 因此只需要更改预定义层的名字
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
+        super(GRU, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        # 实例化预定义的nn.GRU, 它的三个参数分别是input_size, hidden_size, num_layers
+        self.gru = nn.GRU(input_size, hidden_size, num_layers)
+        self.linear = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self, input, hidden):
+        input = input.unsqueeze(0)
+        rr, hn = self.gru(input, hidden)
+        return self.softmax(self.linear(rr)), hn
+
+    def initHidden(self):
+        return torch.zeros(self.num_layers, 1, self.hidden_size)
+
+
+# 因为是onehot编码，输入张量最后一维的尺寸就是n_letters
+input_size = n_letters
+# 定义隐层的最后一维尺寸大小
+n_hidden = 128
+# 输出尺寸为语言类别总数n_categories
+n_categories = len(all_category)
+output_size = n_categories
+# num_layer使用默认值
+num_layer = 1
+
+# 假如我们以一个字母B作为RNN的首次输入, 它通过lineToTensor转为张量
+# 因为我们的lineToTensor输出是三维张量, 而RNN类需要的二维张量
+# 因此需要使用squeeze(0)降低一个维度
+input = lineToTensor('B').squeeze(0)
+# 初始化一个三维的隐层0张量，也是初始的细胞状态张量
+hidden = c = torch.zeros(1, 1, n_hidden)
+
+# 调用
+rnn = RNN(n_letters, n_hidden, n_categories, num_layer)
+lstm = LSTM(n_letters, n_hidden, n_categories, num_layer)
+gru = GRU(n_letters, n_hidden, n_categories, num_layer)
+
+rnn_output, next_hidden = rnn(input, hidden)
+print("rnn:", rnn_output)
+lstm_output, next_hidden, c = lstm(input, hidden, c)
+print("lstm:", lstm_output)
+gru_output, next_hidden = gru(input, hidden)
+print("gru:", gru_output)
