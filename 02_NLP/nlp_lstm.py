3d2342270c84a7839af0b2153404fd5ee5cc9561
yingying <1582137770@qq.com>
2023-02-27 18:05:14 +0800

    add some prompt about rnn

--- 02_NLP/nlp_lstm.py
+++ 02_NLP/nlp_lstm.py
@@ -5,8 +5,11 @@
 
 import torch.nn as nn
 import torch
+# (input_size, hidden_size, num_layers)
 rnn = nn.LSTM(5, 6, 2)
+# (seq_len, batch_size, input_size)
 input = torch.randn(1, 3, 5)
+# (num_layers, batch_size, hidden_size)
 h0 = torch.randn(2, 3, 6)
 c0 = torch.randn(2, 3, 6)
 output, (hn, cn) = rnn(input, (h0, c0))
